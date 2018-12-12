c1eb24bf298e1757f70675d424fb9a9e72fe5a30
Unknown <Jon_athan71@hotmail.com>
2018-12-12 18:00:59 +0100

    almost done with movement and basic rotation logic

--- robotics/__main__.py
+++ robotics/__main__.py
@@ -1,36 +1,35 @@
 from robotics.environment.agent_environment import AgentEnvironment
 from robotics.policy_writers.visual_writer_optimal_path_csv import VisualWriterOptimalPathCSV
 
-""" testing repositioning location & rotation robot"""
+""" test implementation for the repositioning & rotation of our robot"""
+
+
+def print_env_sim(a, rr, df, cs):
+    print 'next action: ', a, \
+        '\tturning this much: {:5} '.format(rr), \
+        '\tcurrent direction:', df, \
+        '\tcurrent state:', cs
 
 
 def run():  # todo: implementing this into agent_dynamics class
-    env = AgentEnvironment(4, 4)
+    env = AgentEnvironment(4, 4, 15)  # row, column, state where treasure is located
     env.fill_optimal_path()
 
-    """first action"""
-    first_action = env.optimal_path[0].action  # first action to start off with
-    rotation_radians = env.rotate(int(first_action))
-
-    action = env.step(first_action)  # get the next action after the first in the optimal path
+    action = int(env.optimal_path[0].action)  # first action to start off with
+    rot_radians = env.rotate(int(action))
+    print_env_sim(action, rot_radians, env.direction_facing, env.current_state)
 
     for _ in range(len(env.optimal_path)):
-        # get the next action in the optimal path,
-        action = env.step(action)
-
+        if env.current_state is env.treasure_state:  # todo: replace with opencv bool
+            break
+        # get the next action and state in the optimal path,
+        next_action = env.step(action)
         # get the amount of radians
-        rotation_radians = env.rotate(action)
-        # env.direction_facing = action
+        rot_radians = env.rotate(int(action))
+        action = next_action  # update action with next action
+        print_env_sim(action, rot_radians, env.direction_facing, env.current_state)
 
-        """
-        loop over all combinations...check optimal path...least favorite
-        """
-        print 'next action: ', action, \
-            '\tturning this much: {:5} '.format(rotation_radians), \
-            '\tcurrent direction:', env.direction_facing, \
-            '\tcurrent state:', env.current_state
-
-    # show optimal path from csv in gui
+    # show optimal path from csv file in gui
     policies = []
     [policies.append(p.action) for p in env.optimal_path]
     VisualWriterOptimalPathCSV.write(policies)
--- robotics/environment/agent_environment.py
+++ robotics/environment/agent_environment.py
@@ -7,15 +7,13 @@ from ai.policy import Policy
 
 class AgentEnvironment:
 
-    def __init__(self, row, column):
+    def __init__(self, row, column, treasure_state):
         self.row = row
         self.column = column
-        self.optimal_path = []
-        # self.optimal_path_2D = numpy.empty((row, column))
-
+        self.treasure_state = treasure_state
         self.current_state = 0
-        self.reward_reached = False
-        self.direction_facing = 2  # the robot front is point to this direction
+        self.optimal_path = []
+        self.direction_facing = 2  # starting direction of robot
 
     """
     robot puts a step in the given action/direction
@@ -25,13 +23,36 @@ class AgentEnvironment:
     def step(self, action):
         # todo: stop when reward boolean is true
         # check for limit
-        if self.current_state <= len(self.optimal_path) - 1:
+        if self.current_state <= (len(self.optimal_path) - 1):
             # get the next state & action by moving the robot
-            next_state, next_action = self.move_robot(action)
+            next_action, next_state = self.move_robot(action)
             self.current_state = next_state  # update state
             self.direction_facing = next_action  # set the action as the current direction of the front of the robot
 
-            return next_action
+            return int(next_action)
+
+    """move in the optimal path array"""
+
+    def move_robot(self, direction):
+        if self.current_state >= len(self.optimal_path) - 1:
+            return 0, 0
+
+        if direction == 0:  # left
+            index = self.current_state - 1
+        elif direction == 1:  # down
+            index = self.current_state + self.row  # length of row
+        elif direction == 2:  # right
+            index = self.current_state + 1
+        elif direction == 3:  # up
+            index = self.current_state - self.row
+        else:
+            index = 0
+
+        # get the action and state at the next position
+        # if its smaller
+        next_action = self.optimal_path[index if index >= 0 else self.current_state].action
+        next_state = self.optimal_path[index if index >= 0 else self.current_state].state
+        return int(next_action), int(next_state)
 
     """returns radians"""
 
@@ -41,9 +62,13 @@ class AgentEnvironment:
             return 0
 
         pos_rotation = False  # negative rotation
-        amount_of_turns = new_direction - self.direction_facing
+        amount_of_turns = self.direction_facing - new_direction
+        if amount_of_turns is 0:
+            return 0
 
-        # change to positive turn
+        self.direction_facing = new_direction
+
+        # change to positive rotation
         if amount_of_turns > 0:
             pos_rotation = True  # positive rotation
 
@@ -55,8 +80,7 @@ class AgentEnvironment:
             target_degrees = 180 if pos_rotation else -180
         elif abs_aot is 3:
             target_degrees = 270 if pos_rotation else -270
-        else:
-            return 0
+
         return target_degrees  # * math.pi / 180 #todo: uncomment to turn on radians
 
     """
@@ -73,28 +97,7 @@ class AgentEnvironment:
     def fill_optimal_path(self):
         with open('../voorbeeld_policy.csv', 'r') as f:
             reader = csv.reader(f)
-            for r in reader:
-                if float(r[2]) > 0.5:
-                    p = Policy(r[0], r[1], r[2])
+            for file_row in reader:
+                if float(file_row[2]) > 0.5:
+                    p = Policy(file_row[0], file_row[1], file_row[2])
                     self.optimal_path.append(p)
-
-    """move in the optimal path array"""
-
-    def move_robot(self, direction):
-        if self.current_state >= len(self.optimal_path) - 1:
-            return 0, 0
-
-        if direction == 0:  # left
-            index = self.current_state - 1
-        elif direction == 1:  # down
-            index = self.current_state + len(self.row)  # length of row
-        elif direction == 2:  # right
-            index = self.current_state + 1
-        elif direction == 3:  # up
-            index = self.current_state - len(self.row)
-        else:
-            index = 0
-
-        next_action = self.optimal_path[index].action
-        next_state = self.optimal_path[index].state
-        return next_action, next_state
