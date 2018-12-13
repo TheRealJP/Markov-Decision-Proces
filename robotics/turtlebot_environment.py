6e02777afb9022ab5eab2138d81a1e4f91038f29
Nick-VO <nick.vanosta@student.kdg.be>
2018-12-13 12:46:17 +0100

    add documentation

--- robotics/turtlebot_environment.py
+++ robotics/turtlebot_environment.py
@@ -3,10 +3,12 @@ from ai.environment import Environment
 
 class TurtleBot(Environment):
     def __init__(self):
+        """The TurtleBot environment. The agent learns to solve a maze."""
         self.__n_states = 0
         self.__n_actions = 4
         self.__current_state = 0
         self.__states = [[]]
+        self.__facing = 0
 
     @property
     def n_states(self):
@@ -25,5 +27,15 @@ class TurtleBot(Environment):
         return self.current_state
 
     def step(self, action):
+        # TODO
+        pass
+
+    def move_forward(self, meters):
+        """Make the TurtleBot move forward a set distance."""
+        # TODO
+        pass
+
+    def turn(self, radians):
+        """Make the TurtleBot turn a set amount of radians."""
         # TODO
         pass
