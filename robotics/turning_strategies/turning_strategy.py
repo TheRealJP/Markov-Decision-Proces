922fe9b44b146e7131fe4587ae37f6f991a26247
Nick-VO <nick.vanosta@student.kdg.be>
2018-12-13 13:01:44 +0100

    add moving and turning strategy interfaces

---
+++ robotics/turning_strategies/turning_strategy.py
@@ -0,0 +1,8 @@
+class TurningStrategy:
+    def turn(self, radians):
+        """
+        Make the TurtleBot turn a set amount of radians.
+        :param radians: angle to turn in radians.
+        """
+        # TODO
+        raise NotImplementedError()
--- robotics/turtlebot_environment.py
+++ robotics/turtlebot_environment.py
@@ -1,14 +1,18 @@
+from math import pi
+
 from ai.environment import Environment
 
 
 class TurtleBot(Environment):
-    def __init__(self):
+    def __init__(self, moving_strategy, turning_strategy):
         """The TurtleBot environment. The agent learns to solve a maze."""
         self.__n_states = 0
         self.__n_actions = 4
         self.__current_state = 0
         self.__states = [[]]
         self.__facing = 0
+        self.__moving_strategy = moving_strategy
+        self.__turning_strategy = turning_strategy
 
     @property
     def n_states(self):
@@ -27,15 +31,19 @@ class TurtleBot(Environment):
         return self.current_state
 
     def step(self, action):
+        """
+        Move in a direction relative to starting position.
+        0: North
+        1: East
+        2: South
+        3: West
+        :param action:
+        :return:
+        """
         # TODO
-        pass
+        # moving forward 1m
+        self.__moving_strategy.move_forward(1)
 
-    def move_forward(self, meters):
-        """Make the TurtleBot move forward a set distance."""
-        # TODO
-        pass
-
-    def turn(self, radians):
-        """Make the TurtleBot turn a set amount of radians."""
-        # TODO
+        # turning 90 degrees
+        self.__turning_strategy.turn(pi / 2)
         pass
