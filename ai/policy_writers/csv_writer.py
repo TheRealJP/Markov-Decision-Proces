acd046c714d4953d18e2152d04626287bfd4ffb9
Unknown <Jon_athan71@hotmail.com>
2019-01-05 16:41:14 +0100

    policywriter needs comma

--- ai/policy_writers/csv_writer.py
+++ ai/policy_writers/csv_writer.py
@@ -8,7 +8,7 @@ class CsvWriter(PolicyWriter):
     @classmethod
     def write(cls, policy):
         # Stringify policy
-        f = '{0};{1};{2}\n'
+        f = '{0},{1},{2}\n'
         output = ''.join([''.join([f.format(s, a, round(policy[s][a], 2)) for a in range(len(policy[s]))])
                           for s in range(len(policy))])
 
